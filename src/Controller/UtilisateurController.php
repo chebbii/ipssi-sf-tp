464ae9f9b6a9bdc79d83b58407a8608771168956
kaouther <chebbiikath@gmail.com>
2019-04-17 16:20:20 +0200

    initial commit

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class UtilisateurController extends AbstractController
+{
+    public function utilisateurs(): Response
+    {
+    	die('ok');
+    }
+}
